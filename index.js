2edb0838a89fac0e712f6df21878ba4e6fd7605e
Shigeyuki Isorena <Isorenashigeyuki21@gmail.com>
2023-04-29 12:24:20 -0700

    adding 404 page and fix typo

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+
+app.set('view engine','jsx')
+app.engine('jsx', require('express-react-views').createEngine());
+
 app.use('/places', require('./controllers/place'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+   res.render('home');
 })
 
 //this catches the 404 error
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 
 app.listen(process.env.PORT)
